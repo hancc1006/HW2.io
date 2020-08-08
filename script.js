6ef4eed5b949fd14806cf70b5bfc9569c6154577
Crys <hancc1006@gmail.com>
2020-08-07 23:20:17 -0700

    update1

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ $(document).ready(function(){
     $('#text1').text("Generate a password at random given the length of the password by the user.");
     $('#text2').text("Try a code quiz");
     $('#text3').text('Check the weather forecast of the city you choose');
+    $('#text4').text('Find recipes from your home');
 
     $('#title1').text('Password Generator');
     $('#title2').text('Code Quiz');
     $('#title3').text('Weather Forecast');
+    $('#title4').text('Recipe Collaboration');
+
+    $("img1").attr("src","./Assets/PWgen.PNG");
+    $("img2").attr("src","./Assets/CodeQuiz.PNG");
+    $("img3").attr("src","./Assets/WeatherForecast.PNG");
+    $("img4").attr("src","./Assets/Collaborative.PNG");
 });
